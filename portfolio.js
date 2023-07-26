020a44dbdae7ff3d2fef40da7805d5da60590129
Gui Prá <gui@guiprav.com>
2023-07-26 05:32:37 -0300

    .

--- portfolio.js
+++ portfolio.js
@@ -2,6 +2,11 @@ let numSteps = 50.0;
 
 let styles = document.createElement('style');
 styles.textContent = `
+  @keyframes fadeIn {
+    0% { opacity:0 }
+    to { opacity:1 }
+  }
+
   @keyframes fadeInDown {
     0% {
       opacity:0;
@@ -13,14 +18,51 @@ styles.textContent = `
     }
   }
 
-  [id] { opacity: 0; transition: 2s ease all }
-  [id].fast { transition: 1s linear all }
-  [id].visible { opacity: 1 }
+  @keyframes fadeInDownLight {
+    0% {
+      opacity:0;
+      transform:translate3d(0,-50%,0)
+    }
+    to {
+      opacity:1;
+      transform:translateZ(0)
+    }
+  }
 
-  [id].visible.fadeDown {
+  [id] { opacity: 0 }
+
+  [id].visible {
+    transition: none;
     animation-name: fadeInDown;
     animation-duration: 2s;
     animation-fill-mode: both;
+  }
+
+  [id].fast { animation-duration: 1s }
+
+  #id0001, #id0002, #id0003 {
+    animation-name: fadeIn;
+  }
+
+  #id0001 {
+    animation-duration: 3s;
+  }
+
+  #id0002 {
+    animation-delay: 2s;
+  }
+
+  #id0003 {
+    animation-delay: 3s;
+  }
+
+  #id0004 {
+    animation-delay: 3s;
+  }
+
+  #id0019, [id="id0023.5"] {
+    animation: none !important;
+    opacity: 1;
   }
 `;
 document.head.append(styles);
