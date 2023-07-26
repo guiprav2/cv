9405d8b453959f2bec94d608f6a299863409ac94
Gui Prá <gui@guiprav.com>
2023-07-26 05:13:40 -0300

    .

--- portfolio.js
+++ portfolio.js
@@ -2,9 +2,26 @@ let numSteps = 50.0;
 
 let styles = document.createElement('style');
 styles.textContent = `
+  @keyframes fadeInDown {
+    0% {
+      opacity:0;
+      transform:translate3d(0,-100%,0)
+    }
+    to {
+      opacity:1;
+      transform:translateZ(0)
+    }
+  }
+
   [id] { opacity: 0; transition: 2s ease all }
   [id].fast { transition: 1s linear all }
   [id].visible { opacity: 1 }
+
+  [id].visible.fadeDown {
+    animation-name: fadeInDown;
+    animation-duration: 2s;
+    animation-fill-mode: both;
+  }
 `;
 document.head.append(styles);
 
